b9acf5a25354497b149dc91dd0a3cb0b1f03efd7
Ahmed Mohammed Yamany <yamany2862016@gmail.com>
2022-11-03 12:46:20 +0200

    Initial Commit

---
+++ Banking App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Banking App
+//
+//  Created by Ahmed Yamany on 03/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
